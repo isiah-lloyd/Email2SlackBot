e2e897b9d4f39625b0c56b1a0f2ef324c3eb9f52
Isiah Lloyd <robotdude101@gmail.com>
2017-02-04 14:38:05 -0500

    Changed Inbox to read only so messages no longer get marked as read

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from pprint import pprint
 import requests
 from email.parser import Parser
+from email.utils import parsedate_tz, mktime_tz
 from ConfigParser import SafeConfigParser
 
 config = SafeConfigParser()
@@ -19,7 +20,7 @@ def getNewEmails(last_uid):
     # Borrowed from Jean-Tiare Le Bigot (https://blog.yadutaf.fr/2013/04/12/fetching-all-messages-since-last-check-with-python-imap/)
     new_messages = 0
     c = email_connection()
-    c.select()
+    c.select('INBOX', readOnly=True)
     command = "{}:*".format(last_uid)
     result, data = c.uid('search', None, 'UID',  command)
     messages = data[0].split()
@@ -45,10 +46,11 @@ def emailParser(raw_email):
         msg_from =  msg['from'][0:]
         msg_subject = msg['subject']
         msg_date = msg['date']
+        date_formatted = mktime_tz(parsedate_tz(msg_date))
         for part in msg.walk():
             if part.get_content_type() == 'text/plain':
                 msg_body = part.get_payload()
-        slackWebHook(msg_subject,msg_from,msg_date,msg_body)
+        #slackWebHook(msg_subject,msg_from,msg_date,msg_body)
 
 def slackWebHook(subject, email_from, date, body):
     payload={"text": "*Subject:* {subject}\n*From:* `{email_from}`\n*Date*: {date}\n--\n{body}".format(subject=subject, email_from=email_from, date=date, body=body)}
@@ -63,7 +65,7 @@ if __name__ == '__main__':
     else:
         print 'Getting UID of first email'
         c = email_connection()
-        c.select()
+        c.select("INBOX", readOnly=True)
         result, data = c.uid('search', None, "ALL")
         c.logout()
         last_uid = data[0].split()[-1]
